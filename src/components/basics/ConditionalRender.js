615a8a5be1816522668b2595c5e03c9d13833b26
Theba Gomez <gomez.teba@gmail.com>
2020-09-26 15:08:08 +0200

    feat(): basic components rendering lesson

---
+++ src/components/basics/ConditionalRender.js
@@ -0,0 +1,8 @@
+import React from 'react'
+import ShowDate from './ShowDate'
+
+const show = true
+
+const ConditionalRender = () => show && <ShowDate />
+
+export default ConditionalRender
---
+++ src/components/basics/Greetings.js
@@ -0,0 +1,6 @@
+import React from 'react' 
+
+const Greetings = () => <h1>Hola! Este es mi primer componente</h1>
+
+export default Greetings
+
