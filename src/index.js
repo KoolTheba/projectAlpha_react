7f8f94fd14be7bd6039010e4f0b8f12e0e358e1a
Theba Gomez <gomez.teba@gmail.com>
2020-10-27 14:36:11 +0100

    feat(): Simplified root index.js file

--- src/index.js
+++ src/index.js
@@ -2,18 +2,11 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import './index.css'
 import App from './App'
-import ApolloClient from '@apollo/client'
-import { ApolloProvider } from '@apollo/client'
 
-const client = new ApolloClient({
-  uri: 'https://rickandmortyapi.com/graphql',
-});
 
 ReactDOM.render(
   <React.StrictMode>
-    <ApolloProvider client={client}>
       <App />
-    </ApolloProvider>
   </React.StrictMode>,
   document.getElementById('root')
 );
