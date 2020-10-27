79fec7f20c43f4dd82a2fbf43e0c889d68f8230b
Theba Gomez <gomez.teba@gmail.com>
2020-10-27 13:11:15 +0100

    feat(): Created Apollo client in the React application and added provider

--- src/index.js
+++ src/index.js
@@ -1,11 +1,19 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import './index.css'
+import App from './App'
+import ApolloClient from '@apollo/client'
+import { ApolloProvider } from '@apollo/client'
+
+const client = new ApolloClient({
+  uri: 'https://rickandmortyapi.com/graphql',
+});
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <ApolloProvider client={client}>
+      <App />
+    </ApolloProvider>
   </React.StrictMode>,
   document.getElementById('root')
 );
